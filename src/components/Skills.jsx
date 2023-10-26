3336b33ebb71afb8e8e0e65cf91f99c3a04382ee
Anurag Affection <anuragaffection07@gmail.com>
2023-10-26 19:38:31 +0530

    updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,7 @@ import { skills } from '../data/Skills';
 const container = `bg-gray-800 text-gray-100`;
 const wrapper = `flex flex-col justify-center items-center p-4 gap-4`;
 const skillContainer = 'flex flex-col p-2 md:p-3 md:grid md:gap-2 grid-cols-2 grid-rows-auto' ;
-const skillStyle = 'flex flex-col justify-center items-center bg-gray-950 p-2 md:p-3 m-1 md:m-3 rounded-xl';
+const skillStyle = 'flex flex-col justify-center items-center bg-gray-950 p-2 md:p-3 m-2 md:m-3 rounded-xl';
 const skillTitle = 'text-yellow-500 text-xl text-center mb-3';
 const skillList = 'flex flex-row flex-wrap gap-3 justify-center items-center p-3';
 const oneSkillStyle = 'flex flex-row justify-center items-center p-2 bg-gray-950 border-gray-700 border-2 rounded-xl gap-2';
@@ -25,12 +25,12 @@ function Skills() {
           <div className={skillContainer}>
             {
               skills.map((oneSection) => (
-                <div className={skillStyle}>
+                <div className={skillStyle} key={oneSection.id}>
                   <div className={skillTitle}>{oneSection.title}</div>
                   <div className={skillList}>
                     {
                       oneSection.skill.map((item) => (
-                        <div className={oneSkillStyle}>
+                        <div className={oneSkillStyle} key={item.id}>
                           <img className={skillImage} src={item.image} alt="" />
                           <span className={skillName}>{item.name}</span>
                         </div>
