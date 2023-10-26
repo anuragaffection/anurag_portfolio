5879953a0c14cfef250bc0c22ab4c9fe1f34b5d4
Anurag Affection <anuragaffection07@gmail.com>
2023-10-26 18:41:33 +0530

    updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { skills } from '../data/Skills';
 
-const container = `bg-gray-900 text-gray-200`;
+const container = `bg-gray-700 text-gray-100`;
 const wrapper = `flex flex-col justify-center items-center p-4 gap-4`;
-const skillContainer = 'flex flex-col bg-dark p-3 md:grid md:gap-2 grid-cols-2 grid-rows-auto' ;
-const skillStyle = 'flex flex-col justify-center items-center bg-gray-950 p-3 m-3 rounded-xl';
+const skillContainer = 'flex flex-col p-2 md:p-3 md:grid md:gap-2 grid-cols-2 grid-rows-auto' ;
+const skillStyle = 'flex flex-col justify-center items-center bg-gray-950 p-2 md:p-3 m-2 md:m-3 rounded-xl';
 const skillTitle = 'text-yellow-500 text-xl text-center mb-3';
 const skillList = 'flex flex-row flex-wrap gap-3 justify-center items-center p-3';
 const oneSkillStyle = 'flex flex-row justify-center items-center p-2 bg-gray-950 border-gray-700 border-2 rounded-xl gap-2';
 const skillImage = 'h-8 w-auto';
-const skillName = 'text-gray-400';
+const skillName = 'text-gray-300';
 
 function Skills() {
   return (
