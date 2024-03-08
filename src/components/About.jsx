c402d2d01fb0e64cb51cce3bf9125625c932b830
Anurag Affection <anuragaffection07@gmail.com>
2024-03-09 00:52:12 +0530

    resume & download icons added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import anuragImage from '../assets/anuragImage.jpg'
 import ResumeAnurag from '../assets/ResumeAnurag.pdf'
 import { ReactTyped } from 'react-typed'
+import {FaFileDownload} from 'react-icons/fa'
 import { Bio } from '../data/Bio'
 
 const container = 'bg-gradient-to-b from-slate-950 to-slate-900 text-gray-100 text-xl p-8 md:px-24 md:py-20 flex flex-col md:flex-row-reverse gap-6 justify-center items-center'
@@ -15,6 +16,7 @@ const typedWrapper = 'text-xl font-semibold mt-2'
 const aStyle = 'font-bold mr-2'
 const typedStyle = 'text-xl text-gray-950 p-1 px-4 rounded bg-gradient-to-l from-amber-600 to-lime-600'
 const btnStyle = 'w-1/2 md:w-1/3 bg-gradient-to-r from-cyan-500 to-blue-500 shadow hover:shadow-cyan-400 duration-500 text-gray-950 font-semibold rounded-lg p-3 px-5'
+const linkStyle = 'flex justify-center items-center gap-3'
 
 
 function About() {
@@ -50,7 +52,7 @@ function About() {
         <button
           className={btnStyle}
         >
-          <a href={ResumeAnurag} download='ResumeAnurag.pdf' target="_blank">Resume</a>
+          <a className={linkStyle} href={ResumeAnurag} download='ResumeAnurag.pdf' target="_blank">Resume <FaFileDownload/> </a>
         </button>
       </div>
     </div>
