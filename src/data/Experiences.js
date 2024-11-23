3f1f45a5fbe4353222ab985ef6522c8976d59a42
Anurag Affection <anuragaffection07@gmail.com>
2024-11-23 15:39:18 +0530

    updated

--- src/data/Experiences.js
+++ src/data/Experiences.js
@@ -1,40 +1,50 @@
-import anuragaffection from '../assets/anuragaffection.jpg'
 
 export const experiences = [
     {
-        id: 0,
+        id: 1,
+        img: "https://media.licdn.com/dms/image/v2/D4E0BAQGE7KTt4GryrQ/company-logo_200_200/company-logo_200_200/0/1688934775740?e=2147483647&v=beta&t=ITlInz_OUhcM9wqvyLrI6gFiDlusHcRR6dJUoZxFXL0",
+        role: "Software Developer",
+        company: "ZEROKELVIN",
+        date: "May 2024 - Present",
+        "desc": "Responsible for end-to-end development, starting from data modeling to writing APIs in Golang, building the frontend with Next.js & Flutter, and deploying branches on AWS and Hetzner VPS. Additionally, set up GitHub Actions and Docker for CI/CD pipelines.",
+        skills: [
+            "NextJs",
+            "Golang",
+            "Flutter",
+            "AWS",
+            "CICD",
+            "Firebase",
+        ],
+    },
+    {
+        id: 2,
+        img: "https://media.licdn.com/dms/image/v2/D560BAQHJCq0bQ3skYw/company-logo_200_200/company-logo_200_200/0/1711567924981/namaste_cloud_logo?e=2147483647&v=beta&t=Fb5tRqSIk1JzaOGR_zIYANnJSwqwnfZBQd8eL29raxE",
+        role: "Full Stack Developer",
+        company: "Namaste Cloud",
+        date: "March 2024 - May 2024",
+        desc: "Building an EdTech platform. I've built course page, realtime certificate generation, authentication, integrated a resume builder and much more.",
+        skills: [
+            "Git & Github",
+            "MERN Stack"
+        ],
+    },
+    {
+        id: 3,
         img: "https://media.licdn.com/dms/image/D560BAQELN71l0Md_HQ/company-logo_200_200/0/1681992257351?e=2147483647&v=beta&t=r61sFSG4RDgfymO9V-1Y0fveF6LgZhydTo1s34ggIHc",
         role: "Full Stack Developer",
         company: "AlmaBetter",
         date: "May 2023 - May 2024",
-        desc: "Created, Deployed & Delivered Full Stack Web App Project from Scratch using recent technologies. Make thier web dev material error free. Taken the doubt class of Merge Sort & Redux Toolkit.",
+        desc: "Developed 'Resume Builder' and 'Entertainment App' projects from the ground up using MERN & Next.js, ensuring scalability. Played a key role in optimizing documentation for accuracy and clarity.",
         skills: [
-            "ReactJS",
-            "Redux Toolkit",
-            "NodeJs",
-            "Material UI",
-            "HTML",
-            "CSS",
-            "JavaScript",
+            "MERN Stack",
+            "Redux js",
+            "Javascript",
+            "Responsive Web Desigin",
+            "Tailwind CSS"
         ],
     },
-    // {
-    //     id: 1,
-    //     img: anuragaffection,
-    //     role: "Educator",
-    //     company: "Anurag Affection",
-    //     date: "September 2018 - Present",
-    //     desc: "As a hobby i am helping community by sharing my experience via different social media platform like Youtube, Website, LinkedIn & more.",
-    //     skills: [
-    //         "Java",
-    //         "DSA",
-    //         "C Programming",
-    //         "Explainer",
-    //         "Listener",
-    //     ],
-    // },
     {
-        id: 2,
+        id: 4,
         img: "https://www.techyukti.com/wp-content/uploads/2023/06/ty-logo-png.png",
         role: "Content Writer",
         company: "Tech Yukti",
---
+++ src/data/Volunteering.js
@@ -0,0 +1,18 @@
+
+export const volunteering = [
+    {
+        id: 1,
+        img: "",
+        role: "Educator",
+        company: "Anurag Affection",
+        date: "September 2018 - Present",
+        desc: "As a hobby i am helping community by sharing my experience via different social media platform like Youtube, Website, LinkedIn & more.",
+        skills: [
+            "Java",
+            "DSA",
+            "C Programming",
+            "Explainer",
+            "Listener",
+        ],
+    },
+]
